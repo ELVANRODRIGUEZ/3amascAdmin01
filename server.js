d3b7634ad859ab10dab2f49c97a51facc6fd5bc6
ELVANRODRIGUEZ <elvanovich2000@yahoo.com.mx>
2024-03-21 09:00:23 -0600

    Test for deploying 02.

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ require("dotenv").config();
 const { createServer } = require('node:http');
 const fs = require('fs')
 
-const hostname = 'https://git.heroku.com/admin3amasc.git';
-// const port = process.env.PORT || 3000;
-const port = 3000
+// const hostname = 'https://git.heroku.com/admin3amasc.git';
+const port = process.env.PORT || 3000;
 
 const server = createServer((req, res) => {
   res.statusCode = 200;

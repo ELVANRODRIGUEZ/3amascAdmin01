d82c32e6e780aaddb067c1f7da6e02c0f2741e01
ELVANRODRIGUEZ <elvanovich2000@yahoo.com.mx>
2024-03-21 08:44:19 -0600

    Test for deploying 01.

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+// This loads "dotenv" package so we can use a .env file to store environmental variables.
+// --nmp install dotenv-- is needed to install this package, and we must proceed to create
+// the .env file afterwards.
+require("dotenv").config();
+
 const { createServer } = require('node:http');
 const fs = require('fs')
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = 'https://git.heroku.com/admin3amasc.git';
+// const port = process.env.PORT || 3000;
+const port = 3000
 
 const server = createServer((req, res) => {
   res.statusCode = 200;

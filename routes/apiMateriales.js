185c5f26b06d2f2b4e855243d755f7e15e324728
ELVANRODRIGUEZ <elvanovich2000@yahoo.com.mx>
2024-04-03 20:59:03 -0600

    Server running, routes created and everything commented in the server side.

--- routes/apiMateriales.js
+++ routes/apiMateriales.js
@@ -1,39 +1,129 @@
+// **************************************** IMPORTING PACKAGES.
+
+// This loads "dotenv" package so we can use a .env file to store environmental variables.
+// --nmp install dotenv-- is needed to install this package, and we must proceed to create
+// the .env file afterwards.
 require("dotenv").config();
 
+// **************************************** CREATING CONSTANTS TO STORE FRAMEWORKS,
+// **************************************** MODULES, APP OBJECT, ROUTES AND OTHER UTILITIES OR PACKAGES
+
+// This loads Express, which is a web app framework for Node.js for:
+// 1) Request handling.
+// 2) Response management.
+// 3) Request parsing.
+// 4) Incoming requests routing.
+// 5) RESTful API Development in general.
+// The entire Express framework is represented in the 'express' object,
+// from which we can access all its functionality.
 const express = require("express");
+
+// This create the URL endpoint or router object, which we will configuer to handle
+// CRUD operations over it.
 const router = express.Router();
 
+// This extracts the Pool property from the 'pg' framework. In this particular
+// case, Pool is a class provided by the 'pg' framework, although in the context
+// of a constant created with this syntax, '{Pool}' would refer to a property from'
+// the required object, in this case, the 'pg' object. Since 'pg' has a property
+// whith that name, we frame our constant name with "{}" to indicate the name
+// of the property we want to extract, and assign it to a variable or constant
+// with the exact same name at the same time.
+// 'pg' framework is known as the 'node-postgres' module. It will represent a 
+// client for a PostgreSQL database, and will allow us to interact with it.
+// Pool is a class from 'pg', and it is used to manage connection to a PostgraSQL
+// database through a connection pooling, which is a technique where a pool (group)
+// of database connection is created and maintain, allowing multiple client requests
+// to share and reuse these connections isntead of creating a new connection for
+// each request, improving performance and resource utilizacion.
+// At this point, we are importing a class (the 'Pool' class), and storing it
+// in the constant also called 'Pool'.
 const { Pool } = require('pg');
 
-// Configure the database connection
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL, // Heroku provides this automatically
-    ssl: {
-      rejectUnauthorized: false // Necessary for connecting to Heroku PostgreSQL
-    }
-  });
 
+//  **************************************** CONFIGURING THE POSTGRESQL DATABASE CONNECTIONS
+
+// From the 'Pool' class we imported, now we create the 'pool' instance (object,
+// as the "{}" indicate (This is called "object literals", or "configuration object")).
+// "connectionString" and "ssl" are configuration options of the
+// "Pool" object (named "pool").
+// "connectionString" specifies the string that allows us to connect to the 
+// database. That string might contain all info required to establish the connection,
+// like the password and database name, when in comes in this format:
+// "postgres://username:password@hostname:port/database_name"
+// Or we could also specify it separately.
+// SSL/TLS is commonly used to secure communications between client (Node.js app) and
+// PosgreSLQ server.
+// The second option is "ssl", which manages the SSL/TLS options for the database
+// connection
+// "rejectUnauthorized" is also an option that is part of the Configuration Object
+// that is being the option for the "ssl" option.
+// In this case, we are telling the client to accept connections from unverified
+// servers, however, the only server the database will be talking to is Heroku, 
+// so there is no risk in that, an "rejectUnauthorized: false" is often needed
+// for the Heroku connection to run smoothly.
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, // Heroku provides this automatically
+  ssl: {
+    rejectUnauthorized: false // Necessary for connecting to Heroku PostgreSQL
+  }
+});
+
+
+//  **************************************** SETTING UP THE GET REQUEST
+
+// This configures the endpoint to react to a GET request. It is important to remember that
+// there can be many reactions to the same request, as there are in this app in other modules.
+// This configures the reaction to a GET request made to the root URL, as indicated in the
+// first argument of the '.get' router method. The second arguement indicates what to do, that
+// is, how to react to the GET reques. 
+// In this case, the reaction is an asyncronous arrow function. By using 'async' keyword,
+// we are introducing the possibility of awaiting for some line of code to yield its result
+// before moving on. That we indicate with the keword 'await'. The functions pauses right
+// there until the execution is done, and then continues its execution.
 router.get('/', async (req, res) => {
-    try {
-        console.log('Request received for /api/materiales'); // Log to check if the route is triggered
-        const client = await pool.connect();
-        console.log('Connected to the database'); // Log to check if the database connection is successful
-    
-        const result = await client.query('SELECT * FROM materiales');
-        const materiales = result.rows;
-        
-        client.release();
-        console.log('Released database connection'); // Log to check if the connection is released
-    
-        res.json(materiales);
-        console.log('Sent materiales data as JSON'); // Log to check if JSON response is sent
-    
-        console.log(materiales); // Log the fetched data for further verification
-      } catch (err) {
-        console.error('Error fetching materiales: ', err); // Log any errors that occur
-        res.status(500).json({ error: 'Internal server error' });
-      }
+
+  // To manage errors properly, everything we do in this function we are going to try it in 
+  // order to catch any possible error.
+  try {
+      console.log('Request received for /api/materiales');
+      const client = await pool.connect(); // The function will pause here until we have a connection 
+      // result.
+      console.log('Connected to the database to SELECT MATERIALES');
+      
+      const result = await client.query('SELECT * FROM materiales'); // We query the connection object (wich 
+      // connects to the database) by using the '.query' method, which passes on the SQL command.
+      const materiales = result.rows; // The query result object stores the fetched rows, and they can be
+      // called with the '.rows' property.
+      
+      client.release(); // We release this connection to the pool, which is a good practice to 
+      // avoid a low performance due to hevy traffic in the endpoint. For small applications, this
+      // might not yield any apparent difference, but in heavy traffic applications it does make
+      // a difference. One can see that if manually triggers several requests one after another.
+      // They will start to slow down the response, or won't get any response at all.
+
+      console.log('Released database connection to SELECT MATERIALES');
+      
+      res.json(materiales);// Sending the response in JSON format
+      console.log('Sent "materiales" data as JSON');
+      
+      // console.log(materiales); // Verification Log.
+
+    } catch (err) {
+
+      // We log the error and send a status to the client.
+      console.error('Error fetching unidades: ', err);
+      res.status(500).json({ error: 'Internal server error' });
+
+    }
 })
 
 
+//  **************************************** EXPORTING THE ROUTER
+
+// In Node.js, 'module' is a global object that represents the curren module (.js file).
+// 'module.exports' is a property that is by default empty ({}). That property can have a
+// single value (object), so we assign the desired value, in this case, 'router' object
+// to 'module.exports' so that whenever we require this module, 'router' object will be
+// the exportation. 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+// **************************************** IMPORTING PACKAGES.
+
 // This loads "dotenv" package so we can use a .env file to store environmental variables.
 // --nmp install dotenv-- is needed to install this package, and we must proceed to create
 // the .env file afterwards.
 require("dotenv").config();
+
+// **************************************** CREATING CONSTANTS TO STORE FRAMEWORKS,
+// **************************************** MODULES, APP OBJECT, ROUTES AND OTHER UTILITIES OR PACKAGES
 
 // This loads Express, which is a web app framework for Node.js for:
 // 1) Request handling.
@@ -9,97 +14,102 @@ require("dotenv").config();
 // 3) Request parsing.
 // 4) Incoming requests routing.
 // 5) RESTful API Development in general.
+// The entire Express framework is represented in the 'express' object,
+// from which we can access all its functionality.
 const express = require("express");
+
+// This imports the Node.js Path module, which provides utilities for working
+// file and directory paths.
 const path = require("path");
+
+// Different from the 'express' object, 'express()' creates an instance of an
+// Express Application, which represents our actual Express server.
+// This is the specific object for which we define routes, middleware
+// and other configuration.
 const app = express();
+
+// This extracts the Pool property from the 'pg' framework. In this particular
+// case, Pool is a class provided by the 'pg' framework, although in the context
+// of a constant created with this syntax, '{Pool}' would refer to a property from'
+// the required object, in this case, the 'pg' object. Since 'pg' has a property
+// whith that name, we frame our constant name with "{}" to indicate the name
+// of the property we want to extract, and assign it to a variable or constant
+// with the exact same name at the same time.
+// 'pg' framework is known as the 'node-postgres' module. It will represent a 
+// client for a PostgreSQL database, and will allow us to interact with it.
+// Pool is a class from 'pg', and it is used to manage connection to a PostgraSQL
+// database through a connection pooling, which is a technique where a pool (group)
+// of database connection is created and maintain, allowing multiple client requests
+// to share and reuse these connections isntead of creating a new connection for
+// each request, improving performance and resource utilizacion.
+// At this point, we are importing a class (the 'Pool' class), and storing it
+// in the constant also called 'Pool'.
 const { Pool } = require('pg');
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // Middleware to parse JSON bodies
+
+// Defining the host name and port and storing them as constants.
+// We give two options: either a set value for both of them, or 
+// a value stored as environmental variables that the server will
+// assign by looking for "HEROKU_HOSTNAME" and "PORT" environmental
+// variables.
+const hostname = process.env.HEROKU_HOSTNAME || '127.0.0.1';
+const port = process.env.PORT || 3300;
+
+// These objects import the corresponding files located in '/routes' folder. 
+// Whatever functionality was coded in those files, is get imported and
+// assigned to these objects, from which we can take advantage of it.
 const obrasRouter = require('./routes/apiObras');
 const unidadesRouter = require('./routes/apiUnidades');
 const materialesRouter = require('./routes/apiMateriales');
 
-// Configure the database connection
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Heroku provides this automatically
-  ssl: {
-    rejectUnauthorized: false // Necessary for connecting to Heroku PostgreSQL
-  }
-});
+// **************************************** SETTING UP ROUTES.
 
-// Test the connection
-// pool.query('SELECT * FROM obras', (err, result) => {
-//   if (err) {
-//     console.error('Error fetching data:', err);
-//   } else {
-//     console.log('Data fetched successfully:', result.rows);
-//   }
-//   pool.end(); // Close the connection pool
-// });
+// This tells our app to allow the server using static files located in the
+// 'client/public' folder directory. They will be used as they are.
+// The 'use' method mounts middleware to a specified location in our
+// app folder structure.
+// 'express.static() is a built-in middleware function for serving
+// static files (like HTML and Javascript files). They are static
+// because they don't require server processing.
+// 'path.join() build an absolute path for those files by joining
+// whatever path chunks we indicate as arguments.
+// In this case, it joins the current module's directory name by using
+// '__dirname', and the route 'client/public' afterwards.
+app.use(express.static(path.join(__dirname, 'client/public')));
 
-// Create an API Endpoint on the server side so we can find the fetched data
-// by making a HTTP Get Request from the client side.
-// app.get('/api/obras', async (req, res) => {
-//   try {
-//     console.log('Request received for /api/obras'); // Log to check if the route is triggered
-//     const client = await pool.connect();
-//     console.log('Connected to the database'); // Log to check if the database connection is successful
-
-//     const result = await client.query('SELECT * FROM obras');
-//     const obras = result.rows;
-    
-//     client.release();
-//     console.log('Released database connection'); // Log to check if the connection is released
-
-//     res.json(obras);
-//     console.log('Sent obras data as JSON'); // Log to check if JSON response is sent
-
-//     console.log(obras); // Log the fetched data for further verification
-//   } catch (err) {
-//     console.error('Error fetching obras: ', err); // Log any errors that occur
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
-
+// These lines set up a route for handling API requests.
+// Each route gets its name in the first argument, and 
+// specifies a middleware that will manage those requests, 
+// in the second argument.
+// We are expressing that our 'app' will have those routes.
 app.use('/api/obras',obrasRouter);
 app.use('/api/unidades',unidadesRouter);
 app.use('/api/materiales',materialesRouter);
 
-app.use(express.static(path.join(__dirname, 'client/public')));
-// Define a route for the landing page (index.html)
+
+// **************************************** DEFINING A ROUTE FOR THE LANDING PAGE ("index.html").
+
+// '.get' manages the HTTP GET request made to a route in our app, 
+// in this case, the '/' route, meaning, the URL of our app.
+// '(req,res) => {}' is an arrow function that defines the procedure to follow
+// at the GET request; it has two objects, the request object (req) and the
+// response object ('res'). We are not using the 'req' object here, but need
+// to write it so the function can distinguis between the two object parameters.
+// 'res.sendFile(path)' sends the static file 'index.html' located in the
+// specified path. We are able to send that file only because we let our app
+// to serve static files from that directory on a previous code line.
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/public/index.html'));
 });
 
-// const { createServer } = require('node:http');
-// const fs = require('fs')
+// **************************************** PORT LISTENING.
 
-const hostname = process.env.HEROKU_HOSTNAME || '127.0.0.1';
-const port = process.env.PORT || 3300;
-
-// const server = createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/html');
-//   fs.readFile('client/public/index.html',(error,data) => {
-//     if (error) {
-//         res.writeHead(404)
-//         res.write('Error: File not Found')
-//     } else {
-//         res.write(data)
-//     }
-//     res.end()
-//   })
-// });
-
-
-
-// Heroku dynamically assigns a port to your application and sets the PORT environment variable. 
+// Heroku dynamically assigns a port to our application and sets the PORT environment variable. 
 // We then must use "process.env.PORT" directly to listen on the correct port provided by Heroku. 
-// By removing hostname from the server.listen method, our server will listen on all available network interfaces, 
+// By removing "hostname" argument (leaving just "port") from the server.listen method, our server will listen on all available network interfaces, 
 // which is what Heroku expects.
 app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// })
